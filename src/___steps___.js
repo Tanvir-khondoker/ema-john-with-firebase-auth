64c61b841b476db6d722c0ee8387f84909b1a0cc
Md.Tanvir-Hossain-khondoker <tanvirkhondo21@gmail.com>
2023-04-28 03:08:48 +0600

    login and sign up form created using vanilla css

---
+++ src/___steps___.js
@@ -0,0 +1,7 @@
+/**
+ * 1. create a firebase project
+ * 2.create a web app
+ * 3.npm install firebase
+ * 4.save firebase config and export default app
+ * 5. build> authentication> get started > enable sign in method
+ * **/ 
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,20 @@
+/
+import { initializeApp } from "firebase/app";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBOzrMxeuh_eO87p-2sxDTUecymKxmtdrc",
+  authDomain: "ema-john-with-firebase-a-d27e7.firebaseapp.com",
+  projectId: "ema-john-with-firebase-a-d27e7",
+  storageBucket: "ema-john-with-firebase-a-d27e7.appspot.com",
+  messagingSenderId: "957786347981",
+  appId: "1:957786347981:web:3f8ee0a1545eaf21cc0c84"
+};
+
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
